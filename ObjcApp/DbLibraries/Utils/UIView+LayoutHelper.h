424e01cdb89755baada62fccef97ece728572ddf
Dylan Bui <buivantienduc@yahoo.com>
2017-10-31 18:25:26 +0700

    commit

---
+++ ObjcApp/DbLibraries/Utils/UIView+LayoutHelper.h
@@ -0,0 +1,30 @@
+//
+//  UIView+LayoutHelper.h
+//  ObjcApp
+//
+//  Created by Dylan Bui on 10/31/17.
+//  Copyright © 2017 Propzy Viet Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LayoutHelper)
+
+@property (nonatomic) CGPoint origin;
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+
+@property (nonatomic) CGSize size;
+
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+
+@end
+
+
