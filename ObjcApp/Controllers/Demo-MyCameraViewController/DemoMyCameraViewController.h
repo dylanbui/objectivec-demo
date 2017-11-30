3a49434b22ae4ce9bf90915ca6152da90b4031f3
Dylan Bui <buivantienduc@yahoo.com>
2017-11-30 09:38:09 +0700

    commit

---
+++ ObjcApp/Controllers/Demo-MyCameraViewController/DemoMyCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoMyCameraViewController.h
+//  ObjcApp
+//
+//  Created by Dylan Bui on 11/29/17.
+//  Copyright © 2017 Propzy Viet Nam. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DemoMyCameraViewController : BaseViewController
+
+@end
